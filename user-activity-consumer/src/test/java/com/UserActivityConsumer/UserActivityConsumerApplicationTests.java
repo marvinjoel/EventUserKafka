b58d348535cf6209303cf59e2db511f5f463330c
MarvinAchas <marvinacha@ghlperu.com>
2025-04-04 16:17:48 -0500

    first commit

---
+++ user-activity-consumer/src/test/java/com/UserActivityConsumer/UserActivityConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserActivityConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserActivityConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-activity-producer/src/test/java/com/userActivityProducer/UserActivityProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userActivityProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserActivityProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
