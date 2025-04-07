eeb04b6d5daaccea96b493573fa67cd721b7a4e0
MarvinAchas <marvinacha@ghlperu.com>
2025-04-06 22:58:22 -0500

    Create DTO for data transfer

---
+++ user-activity-producer/src/main/java/com/userActivityProducer/DTO/User.java
@@ -0,0 +1,18 @@
+package com.userActivityProducer.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
