04f634fb1f0fe8f1ddcd044d95c0ffb6a80f16bc
MarvinAchas <marvinacha@ghlperu.com>
2025-04-07 20:02:49 -0500

    .

--- user-activity-producer/src/main/java/com/userActivityProducer/config/StringProducerFactoryConfig.java
+++ user-activity-producer/src/main/java/com/userActivityProducer/config/StringProducerFactoryConfig.java
@@ -9,7 +9,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-eimport org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
